277bb384385d6faeba2b80ab59c1135b8132e4be
Hiroki <face9363>
2020-12-29 00:20:01 +0900

    add customize css

--- src/index.js
+++ src/index.js
@@ -1,22 +1,28 @@
-import { default as Handsontable } from "handsontable";
-import "../assets/handsontable.full.min.css"
+import "../assets/jexcel.css"
+import "../assets/customize.css"
 
-window.onload = () => {
-    var data = [
-        ['', 'Ford', 'Tesla', 'Toyota', 'Honda'],
-        ['2017', 10, 11, 12, 13],
-        ['2018', 20, 11, 14, 13],
-        ['2019', 30, 15, 12, 13]
-    ];
-      
-    var container = document.getElementById('table');
-    var hot = new Handsontable(container, {
-        data: data,
-        rowHeaders: true,
-        colHeaders: true,
-        filters: true,
-        dropdownMenu: true
-    });
-    
+import jexcel from "jexcel"
+
+var data = [
+    ['', 'Ford', 'Tesla', 'Toyota', 'Honda'],
+    ['2017', 10, 11, 12, 13],
+    ['2018', 20, 11, 14, 13],
+    ['2019', 30, 15, 12, 13]
+];
+
+var container = document.getElementById('table');
+
+const table = jexcel(container, {
+    data: data,
+    saveAs: false,
+    onchange(){
+        
+    },
+})
+
+console.log(table.getData())
+
+const sendTable = () =>{
+    return table.getData()
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
       path: path.join(__dirname, 'dist'),
       filename: 'bundle.js'
     },
-    devtool: 'source-map',
+    devtool: 'eval-source-map',
     module: {
       rules: [
         {
